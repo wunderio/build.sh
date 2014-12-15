8d7d3fa1fe5e06773f9d6fe73b43efc45cb24d93
Teemu Merikoski <teemu.merikoski@gmail.com>
2014-12-15 14:43:03 +0200

    Remove memcache and varnish dependencies. User sqlite database instead of mysql

--- conf/local.settings.php
+++ conf/local.settings.php
@@ -3,35 +3,10 @@
  * EXAMPLE SETTINGS FOR LOCAL ENVIRONMENT
  */
 
-
 // DATABASE
 
-$databases = array (
-  'default' => 
-  array (
-    'default' => 
-    array (
-      'database' => 'drupal',
-      'username' => 'drupal',
-      'password' => 'password',
-      'host'     => 'localhost',
-      'port'     => '',
-      'driver'   => 'mysql',
-      'prefix'   => '',
-    ),
-  ),
+$databases['default']['default'] = array(
+  'driver' => 'sqlite',
+  'database' => 'test.sqlite',
 );
-
-// CACHING
-$conf['cache_backends'][] = 'sites/all/modules/contrib/memcache/memcache.inc';
-$conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
-$conf['cache_default_class'] = 'MemCacheDrupal';
-$conf['memcache_key_prefix'] = 'wk';
-
-$conf['cache_backends'][] = 'sites/all/modules/contrib/varnish/varnish.cache.inc';
-$conf['cache_class_cache_page']  = 'VarnishCache';
-
-// Varnish
-$conf['varnish_version'] = "3";
-
 
--- conf/site.make
+++ conf/site.make
@@ -5,10 +5,7 @@ core = 7.0
 api = 2
 
 projects[drupal][type] = core
-projects[drupal][version] = 7.32
-
-; PATCH: user_save might on occasion delete images from users
-projects[drupal][patch][935592] = https://www.drupal.org/files/issues/935592-89.patch
+projects[drupal][version] = 7.34
 
 defaults[projects][subdir] = "contrib"
 
@@ -21,12 +18,10 @@ projects[devel][version] = 1.5
 projects[entity][version] = 1.5
 projects[entity_translation][version] = 1.0-beta3
 projects[field_collection][version] = 1.0-beta7
-projects[memcache][version] = 1.2
 projects[pathologic][version] = 2.12
 projects[rules][version] = 2.7
 projects[search_api][version] = 1.13
 projects[search_api_solr][version] = 1.6
-projects[varnish][version] = 1.0-beta3
 projects[views][version] = 1.2
 projects[views][version] = 3.8
 projects[views_bulk_operations][version] = 3.2
