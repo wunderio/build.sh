643d3aec29ef0b56358d2d79ace90974c84fa06f
Teemu Merikoski <teemu.merikoski@gmail.com>
2015-06-05 13:55:50 +0300

    Place sqlite dump in the root folder

--- conf/local.settings.php
+++ conf/local.settings.php
@@ -5,10 +5,14 @@
 
 // DATABASE
 
+$db_filename = dirname(__FILE__);
+$db_filename = explode("/", $db_filename);
+array_pop($db_filename);
+$db_filename = implode('/', $db_filename) . '/drupal.sqlite';
+
 $databases['default']['default'] = array(
   'driver' => 'sqlite',
-  'database' => 'test.sqlite',
+  'database' => $db_filename,
 );
-
 
 ini_set('memory_limit','1024M');
