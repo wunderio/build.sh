f266fba4df7c2cb80f013c0189a8fc244b5f8ac2
Teemu Merikoski <teemu.merikoski@gmail.com>
2015-09-10 10:30:03 +0300

    D8 version fixes

--- conf/local.settings.php
+++ conf/local.settings.php
@@ -12,9 +12,15 @@ $db_filename = implode('/', $db_filename) . '/drupal.sqlite';
 
 $databases['default']['default'] = array(
   'driver' => 'sqlite',
+  'prefix' => '',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\sqlite',
   'database' => $db_filename,
 );
 
 ini_set('memory_limit','1024M');
 
 $conf['drupal_http_request_fails'] = FALSE;
+
+################################################################################
+# Site install will inject settings below this line
+################################################################################
