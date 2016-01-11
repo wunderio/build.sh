7511902998778f69478f9825479671aab060f34c
Teemu Merikoski <teemu.merikoski@wunderkraut.com>
2016-01-11 14:03:48 +0200

    Default installer configs to be used with config_installer. Removed install wk profile.

--- code/profiles/README.txt
+++
@@ -1,1 +0,0 @@
-Place any custom profiles in their respective subdirectories in this folder.
--- conf/local.settings.php
+++ conf/local.settings.php
@@ -10,26 +10,17 @@ $db_filename = explode("/", $db_filename);
 array_pop($db_filename);
 $db_filename = implode('/', $db_filename) . '/drupal.sqlite';
 
-$databases['default']['default'] = array(
+$databases['default']['default'] = [
   'driver' => 'sqlite',
   'prefix' => '',
   'namespace' => 'Drupal\\Core\\Database\\Driver\\sqlite',
   'database' => $db_filename,
-);
+];
 
 ini_set('memory_limit','1024M');
 
 $conf['drupal_http_request_fails'] = FALSE;
 
-################################################################################
-# Site install will inject settings below this line
-################################################################################
-$databases['default']['default'] = array (
-  'database' => '/Users/tcmug/www/build.sh/drupal.sqlite',
-  'prefix' => '',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\sqlite',
-  'driver' => 'sqlite',
-);
+$settings['install_profile'] = 'config_installer';
+$config_directories['sync'] = DRUPAL_ROOT . '/../config/sync';
 $settings['hash_salt'] = '_Yfo_5ZbqO2oOOrypjh7QlMMmnpkeA2_Egvdn09ZTAa3ajv19494SKvJ06NZcsdb_2piGYqqGA';
-$settings['install_profile'] = 'wk';
-$config_directories['sync'] = 'sites/default/files/config_lJUh5WPtm0SacTKwhSYOhYRInRda6YfXkcC93SP26W_m2UMbC6rfLpq2lX3PEaa9cCgld4DzXA/sync';
